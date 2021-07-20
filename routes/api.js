aaed40257e65d1c71f55cb81096c6428bec68fae
ayfor <josh.a.stubbington@gmail.com>
2021-07-19 23:00:59 -0400

    added route for recent workouts

--- routes/api.js
+++ routes/api.js
@@ -54,4 +54,23 @@ router.put('/api/workouts/:id', (req, res) => {
     })
 });
 
+//GET RECENT WORKOUTS
+router.get('/api/workouts/range', (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" }
+            },
+        },
+    ])
+    .sort({day:-1})
+    .limit(7)
+    .then(dbTransaction => {
+        res.json(dbTransaction);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
+});
+
 module.exports = router;

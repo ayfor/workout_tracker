b3d96954de12fbb5c0b6b253c051f796a2a8db0c
ayfor <josh.a.stubbington@gmail.com>
2021-07-19 21:01:02 -0400

    consolidated exercise and workout model

--- models/workout.js
+++ models/workout.js
@@ -10,9 +10,34 @@ const workoutSchema = new Schema({
     },
     exercises: [
         {
-            type: Schema.Types.ObjectId,
-            ref: "Exercise"
-        }
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter the type of workout"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter a name for this workout"
+        },
+        duration: {
+            type: Number,
+            required: "Enter a duration for the workout"
+        },
+        weight:{
+            type: Number
+        },
+        reps:{
+            type: Number
+        },
+        sets:{
+            type: Number
+        },
+        distance:{
+            type: Number
+        },
+
+    }
     ]
 })
 

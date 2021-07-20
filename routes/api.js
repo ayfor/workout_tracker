c79b9ab5e0472a73fa5ac143cbcc724d979ac2cd
ayfor <josh.a.stubbington@gmail.com>
2021-07-19 22:09:14 -0400

    added initial api routes and server config

--- routes/api.js
+++ routes/api.js
@@ -8,4 +8,26 @@ router.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index
 router.get('/exercise', (req, res) => res.sendFile(path.join(__dirname, '../public/exercise.html')));
 router.get('/stats', (req, res) => res.sendFile(path.join(__dirname, '../public/stats.html')));
 
+//-----API ROUTES-----
+
+router.get('/api/workouts', (req, res) => {
+    Workout.find({})
+        .then(dbTransaction => {
+            res.json(dbTransaction);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
+})
+
+router.post('/api/workouts', ({body},res) => {
+    Workout.create(body)
+        .then(dbTransaction => {
+            res.json(dbTransaction);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
+})
+
 module.exports = router;
